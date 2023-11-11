36ab387cd20bea545f93aa99bded5781c1adea9c
unknown <brassare@hotmail.com>
2023-11-11 11:59:39 -0300

    incluindo a tabela de vendas

--- pages/02_🏢_stores.py
+++ pages/02_🏢_stores.py
@@ -4,4 +4,8 @@ import pandas as pd
 st.set_page_config(page_title="stores", page_icon="🏢", layout= "wide")
 
 df_lojas = pd.read_csv(r"C:\Users\aluno\Documents\project python\database\Lojas.csv", sep=";", encoding="latin1", index_col="ID Loja", parse_dates= True)
+
 df_lojas
+
+df_vendas = pd.read_excel(r"C:\Users\aluno\Documents\project python\database\Vendas.xlsx")
+df_vendas
