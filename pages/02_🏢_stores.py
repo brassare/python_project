13e7bfccd22b54130f9c165693f4734b27cf6e43
unknown <brassare@hotmail.com>
2023-11-11 12:09:22 -0300

    alterando markdown

--- pages/02_🏢_stores.py
+++ pages/02_🏢_stores.py
@@ -3,12 +3,18 @@ import pandas as pd
 
 st.set_page_config(page_title="stores", page_icon="🏢", layout= "wide")
 
+
 df_lojas = pd.read_csv(r"C:\Users\aluno\Documents\project python\database\Lojas.csv", sep=";", encoding="latin1", index_col="ID Loja", parse_dates= True)
 
-df_lojas
+
 
 df_vendas = pd.read_excel(r"C:\Users\aluno\Documents\project python\database\Vendas.xlsx")
-df_vendas
+
 
 lojas = df_lojas['Loja'].unique()
 loja = st.sidebar.selectbox("Loja", lojas)
+
+st.markdown(f"# {loja}")
+
+df_lojas
+df_vendas
